d87787fe86d90316721a4782d529e84e77738d23
Eric Le Vieux <eric.levieux1@gmail.com>
2026-03-31 13:55:33 +0400

    Move tokens to GitHub Secrets

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Update these values if your API key or Base ID changes
 
 const AIRTABLE_CONFIG = {
-  apiKey: "patc9kMQEbNlO3fq6.8c2b50a1a8bbd1a38bd87b43e2a17846ef16f35c569f8fa0a617556c72fe2d2e",  // Replace with your Personal Access Token
+  apiKey: "AIRTABLE_TOKEN_PLACEHOLDER",  // Replace with your Personal Access Token
   baseId: "appFms83tvOuiMDsA",
   tables: {
     opportunities: "Opportunities",
